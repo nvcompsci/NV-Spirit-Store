b3d62bec759cc1a2ccd11c5ad1c1b64e94925226
John Word <jword@sylvaniaschools.local>
2020-01-21 14:27:47 -0500

    more info in cart

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
 const $cart = document.querySelector("section#cart ul")
+const inCart = []
+let total = 0
 
 let items = []
 items[0] = {
@@ -16,8 +18,13 @@ items[1] = {
 function addToCart(id) {
     const item = items.find(item => item.id == id)
     
-    alert(item.name)
+    //alert(item.name)
     const $newItem = document.createElement("li")
-    $newItem.innerHTML = item.name
+    $newItem.innerHTML = 
+        `${item.name} ( $ ${item.price} )`
     $cart.append($newItem)
+    inCart.push(item)
+    total += item.price
+    document.querySelector("span#total").innerHTML = total
+    document.querySelector("span#itemCount").innerHTML = inCart.length
 }
