993a124a56a05b99ffbe2cce212583a9de649d63
John Word <jword@sylvaniaschools.local>
2020-01-21 14:02:34 -0500

    item added to cart

--- public/script.js
+++ public/script.js
@@ -1,3 +1,23 @@
+const $cart = document.querySelector("section#cart ul")
+
+let items = []
+items[0] = {
+    id: 1,
+    name: "Programming T-Shirt",
+    price: 10.99
+}
+
+items[1] = {
+    id: 2,
+    name: "Chess Club T-Shirt",
+    price: 14.99
+}
+
 function addToCart(id) {
-    alert(id)
+    const item = items.find(item => item.id == id)
+    
+    alert(item.name)
+    const $newItem = document.createElement("li")
+    $newItem.innerHTML = item.name
+    $cart.append($newItem)
 }
