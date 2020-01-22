038ca127abc890f9aaac9b1b80ed33d777a8c2f3
John Word <jword@sylvaniaschools.local>
2020-01-22 13:34:54 -0500

    more details, checkout started

--- public/script.js
+++ public/script.js
@@ -15,16 +15,17 @@ items[1] = {
     price: 14.99
 }
 
-function addToCart(id) {
+function addToCart(id, event) {
     const item = items.find(item => item.id == id)
-    
+    const size = event.target.parentNode
+        .querySelector("select").value
     //alert(item.name)
     const $newItem = document.createElement("li")
     $newItem.innerHTML = 
-        `${item.name} ( $ ${item.price} )`
+        `${item.name} [ ${size} ] ( $ ${item.price} )`
     $cart.append($newItem)
     inCart.push(item)
     total += item.price
-    document.querySelector("span#total").innerHTML = total
+    document.querySelector("span#total").innerHTML = total.toFixed(2)
     document.querySelector("span#itemCount").innerHTML = inCart.length
 }
