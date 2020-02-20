1d7e8c934641626f746b2f1489eda2da73c8363e
John Word <jword@sylvaniaschools.local>
2020-02-20 11:51:13 -0500

    order stores in database correctly

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use( express.static('public') )
 
 app.get("/items", (req, res) => {
-    let sql = `SELECT * FROM items`;
+    let sql = `SELECT rowid as itemid, name, price, image, inventory  FROM items`;
 
     db.all(sql, [], (err, rows) => {
         if (err) {
@@ -28,6 +28,9 @@ app.get("/items", (req, res) => {
 })
 
 app.post("/order", (req, res) => {
+    //make all db statements run in series/order
+    db.serialize(() => {
+        
     //req.body is the data user sends
     console.log(req.body)
     const customer = req.body.user
@@ -35,24 +38,37 @@ app.post("/order", (req, res) => {
 (firstname, lastname, address, email) 
 VALUES (?, ?, ?, ?)`
     const values = [customer.first, customer.last, customer.address, customer.email]
-    db.run(sql, values, (err) => {
+    
+    db.run(sql, values, function (err) {
         console.log(err)
-    })
+        console.log(`customerid ${this.lastID} created`)
+        let customerID = this.lastID
+    
     
     //create new invoice
-    const sql2 = `INSERT INTO invoices (customerid, total, orderdate) VALUES (?, ?, ?, ?)`
-    const values2 = [1,req.body.total, req.body.orderDate]
-    db.run(sql2, values2, (err) => {
+    const sql2 = `INSERT INTO invoices (customerid, total, orderdate) VALUES (?, ?, ?)`
+    const values2 = [customerID,req.body.total, req.body.orderDate]
+    
+    db.run(sql2, values2, function (err) {
         console.log(err)
-    })
+        let invoiceID = this.lastID        
+        console.log(`invoiceid ${this.lastID} created`)
+    
     //is there inventory left for this item?
     const sql3 = `SELECT inventory FROM items WHERE itemid = ?`
     
     //associate items with invoice
-    const sql4 = `INSERT INTO invoice_item (invoiceid, itemid) values (?, ?)`
-    const values4 = [1, req.body.items[0].itemid]
+    //create string like (?, ?) for each item
+    const placeholders = req.body.items.map(item => `(${invoiceID}, ?)`).join(', ')
+    const sql4 = `INSERT INTO invoice_item (invoiceid, itemid) values ${placeholders}`
+    //map/transform all item details to array of just itemid's
+    const values4 = req.body.items.map(item => item.itemid)
     db.run(sql4, values4, (err) => {
         console.log(err)
+    })
+    })
+    })
+        
     })
 })
 
